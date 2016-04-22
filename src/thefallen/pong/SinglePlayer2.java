4e498f86a594a53b20c4eff765ad1d307e17ed83
Saurabh Kumar <saurabhkumar1311@gmail.com>
2016-04-22 19:56:01 +0530

    ew

---
+++ src/thefallen/pong/SinglePlayer2.java
@@ -0,0 +1,134 @@
+package thefallen.pong;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import static com.sun.glass.ui.Cursor.setVisible;
+import static java.lang.System.err;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+/**
+ * Created by saurabh on 4/14/2016.
+ */
+
+public class SinglePlayer2 {
+
+    pong game;
+    int N;
+    int uHP = 100;
+    static int init_num = 7;
+    static int init_hp = 0;
+    boolean pause_flag = false,constructing;
+    SinglePlayer2 quest;
+    Ball.onDiedListener diedListener = new Ball.onDiedListener() {
+        @Override
+        public void onDied(int index, SinglePlayer lol) {
+            out.println(index+":  YOU DIED");
+            if(N>2&&!constructing&&index!=0)
+            {
+                err.println("started constructing");
+                uHP = game.rackets[0].hp;
+                pause();
+                startGame(N-1);
+                N--;
+                constructing=true;
+            }
+            else if(index==0)
+            {
+                out.println("YOU DIED");
+            }
+
+        }
+    };
+    public SinglePlayer2()
+    {
+        quest = this;
+    }
+    public void pressed(int e)
+    {
+        if(e == KeyMap.exit)
+        {
+            pause();
+        }
+    }
+
+    void pause()
+    {
+        if(pause_flag)
+        {
+            pause_flag = false;
+            game.f_balls.get(0).dt = 1;
+            game.rackets[0].dt = 0.5f;
+            out.println("cdsjhkc");
+        }
+        else
+        {
+            pause_flag = true;
+            game.f_balls.get(0).dt = 0;
+            game.rackets[0].dt = 0f;
+            out.println("jhdcj");
+        }
+
+    }
+
+    void startGame(int n)
+    {
+        System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                game = new pong(n);
+                game.onDiedListener = diedListener;
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        game.addBall();
+                        game.f_balls.get(0).vx = 6;
+                        game.f_balls.get(0).vy = 6;
+                        constructing = false;
+                        err.println("constructing done");
+                        for (int i = 0; i < n; i++) {
+                            if (i == 0) {
+                                game.rackets[0].hp = uHP;
+                            } else {
+                                game.rackets[i].hp = init_hp;
+                            }
+                        }
+                        game.f_frame.addKeyListener(new KeyListener() {
+                            @Override
+                            public void keyTyped(KeyEvent e) {
+                            }
+
+                            @Override
+                            public void keyPressed(KeyEvent e) {
+                                pressed(e.getKeyCode());
+                            }
+
+                            @Override
+                            public void keyReleased(KeyEvent e) {
+
+                            }
+                        });
+                    }});
+            }
+        });
+
+    }
+
+
+    public void startQuest()
+    {
+        N = init_num;
+        startGame(N);
+    }
+
+    static public void main(String[] args)
+    {
+        SinglePlayer2 quest = new SinglePlayer2();
+        quest.startQuest();
+    }
+}
