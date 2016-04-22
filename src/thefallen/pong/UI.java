b537c6b22d1f1266970eac89fc5b408d3395c54f
Anil Kumar Meena <anil4514@gmail.com>
2016-04-22 19:27:32 +0530

    First UI commit

--- src/thefallen/pong/UI.java
+++ src/thefallen/pong/UI.java
@@ -3,32 +3,109 @@ package thefallen.pong;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class UI extends Application {
+
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+
+        primaryStage.setTitle("Ping Pong!");
+
+        GridPane landingHeader= getheader("Ping Pong");
+        //test
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        Scene sc = new Scene(grid, 600, 330,Color.AQUA);
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+                primaryStage.setScene(sc);
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        //test
+
+        BorderPane border = new BorderPane();
+        border.setTop(landingHeader);
+        border.setCenter(grid);
+        border.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #dc143c, #661a33)");
+
+        Scene scene = new Scene(border, 600, 330,Color.AQUA);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    public GridPane getheader(String title){
+        GridPane header = new GridPane();
+        header.setPadding(new Insets(5));
+        header.setHgap(10);
+        header.setVgap(10);
+
+        final ImageView imv = new ImageView();
+        final Image image2 = new Image(UI.class.getResourceAsStream("res/logo_small.png"));
+        imv.setImage(image2);
+        Text tit = new Text(title);
+        tit.setFont(Font.font("ZinHenaBokuryu-RCF", FontWeight.NORMAL, 20));
+
+        final HBox pictureRegion = new HBox();
+
+        pictureRegion.getChildren().addAll(imv,tit);
+        header.add(pictureRegion, 1, 1);
+        return header;
+    }
 }
+
