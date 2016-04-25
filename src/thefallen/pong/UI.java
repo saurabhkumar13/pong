6fdd7eb823720ac7be3b41f4b3f45376651288bd
Anil Kumar Meena <anil4514@gmail.com>
2016-04-25 15:08:03 +0530

    mew

--- src/thefallen/pong/UI.java
+++ src/thefallen/pong/UI.java
@@ -854,7 +854,7 @@ public class UI extends Application {
             @Override
             public void handle(ActionEvent e) {
                 if(passField.getText().equals(pass))
-                    pee.joinserver(player_name,element,IP,mode);
+                    pee.joinserver(player_name,element,IP);
             }
         });
         HBox hBox = new HBox();
